5c8e15de4bc47913403c636dce28e4204157a04e
MarkiDev <markitolaga@gmail.com>
2025-05-12 01:22:58 -0600

    correeccionees y cambios para ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-const base = '/Invitacio_Italia/';
+const repoName = 'Invitacion_Italia'; // Asegúrate de que este es el nombre exacto de tu repositorio GitHub
+const base = `/${repoName}`; 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://MarkianUchiha.github.io',
+  site: 'https://MarkianUchiha.github.io', // Reemplaza 'MarkianUchiha' con tu nombre de usuario de GitHub
   base: base,
+  build: {
+    assetsPrefix: base, // ¡CRUCIAL para las rutas de GitHub Pages!
+  },
   vite: {
     plugins: [tailwindcss()]
   }
