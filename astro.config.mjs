245f707d210afdf82d4053c53a0358015892a69c
MarkiDev <markitolaga@gmail.com>
2025-05-11 21:15:27 -0600

    cambios en config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const base = '/Invitacio_Italia/';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MarkianUchiha.github.io',
+  base: base,
   vite: {
     plugins: [tailwindcss()]
   }
